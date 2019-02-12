c8085a85568b90ec2a7d2a55081756b4f14c61d4
miyass <exe.miyakura0629@gmail.com>
2019-02-12 13:24:25 +0900

    Initial Commit

---
+++ ventaku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ventaku
+//
+//  Created by 宮倉宗平 on 2019/02/12.
+//  Copyright © 2019 Sohei Miyakura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
